c2b9fe355129b55208279a6969c9aeac389150ae
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-11-20 22:05:38 -0800

    add connect up extension

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,60 +1,54 @@
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
+  const [luksoAvailable, setLuksoAvailable] = useState(false);
+
+  const [builders, setBuilders] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLuksoAvailable(!!window.lukso);
+  }, []);
+
+  useEffect(() => {
+    fetch("https://buidlguidl-v3.ew.r.appspot.com/builders")
+      .then(res => res.json())
+      .then(data => {
+        setBuilders(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!builders) return <p>No profile data</p>;
+
+  console.log(builders);
+
+  async function connect() {
+    if (window.lukso) {
+      try {
+        const accounts = await window.lukso.request({ method: "eth_requestAccounts" });
+        console.log("accounts", accounts);
+      } catch (error) {
+        console.error("Error connectinng to LUKSO", error);
+      }
+    }
+  }
+
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
-        </div>
+      <h1 className="text-5xl text-center my-14">UPdev</h1>
+      <div className="flex items-center flex-col flex-grow">
+        {luksoAvailable ? (
+          <button className="btn btn-accent btn-lg capitalize" onClick={() => connect()}>
+            Connect UP Extension
+          </button>
+        ) : (
+          <p>You must install the lukso extension before using this dapp</p>
+        )}
       </div>
     </>
   );
