e0042bc314e902012e0ed5061f00c5267c552e42
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-11-22 20:15:42 -0800

    theme colors and landing page

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -40,7 +40,7 @@ export const RainbowKitCustomConnectButton = () => {
             {(() => {
               if (!connected) {
                 return (
-                  <button className="btn btn-primary btn-sm" onClick={openConnectModal} type="button">
+                  <button className="btn btn-accent capitalize text-lg px-6" onClick={openConnectModal} type="button">
                     Connect Wallet
                   </button>
                 );
