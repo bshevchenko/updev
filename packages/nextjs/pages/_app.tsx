690a76fa7cd34e4e4c0a6348e95be5b6fbb3e00c
Boris Shevchenko <bshevchenkoweb@gmail.com>
2024-04-30 18:26:46 +0300

    removed excess files

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
-import { UniversalProfileProvider } from "../providers/UniversalProfile";
+import { useEffect, useState } from "react";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";
 import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
 import { Toaster } from "react-hot-toast";
 import { useDarkMode } from "usehooks-ts";
 import { WagmiConfig } from "wagmi";
-// import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
@@ -16,10 +13,12 @@ import { useGlobalState } from "~~/services/store/store";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 import { appChains } from "~~/services/web3/wagmiConnectors";
 import "~~/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
 const ScaffoldEthApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   const price = useNativeCurrencyPrice();
   const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
+  
   // This variable is required for initial client side rendering of correct theme for RainbowKit
   const [isDarkTheme, setIsDarkTheme] = useState(true);
   const { isDarkMode } = useDarkMode();
@@ -49,7 +48,6 @@ const ScaffoldEthApp = ({ Component, pageProps: { session, ...pageProps } }: App
             <main className="relative flex flex-col flex-1">
               <Component {...pageProps} />
             </main>
-            {/* <Footer /> */}
           </div>
           <Toaster />
         </RainbowKitProvider>
