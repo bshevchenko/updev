069f29e91f0c0ac7c804ece8c7b499bb63142adb
Boris Shevchenko <bshevchenkoweb@gmail.com>
2024-05-28 12:04:20 +0300

    chainlink serverless workaround

--- packages/nextjs/lib/don.ts
+++ packages/nextjs/lib/don.ts
@@ -1,6 +1,7 @@
-import { signer } from "./signer";
-import { SecretsManager, createGist } from "@chainlink/functions-toolkit";
+// import { signer } from "./signer";
+// import { SecretsManager, createGist } from "@chainlink/functions-toolkit";
 import latestVersions from "~~/latest.json";
+import axios from "axios";
 
 export type PreparedRequest = {
   secret: string;
@@ -12,19 +13,27 @@ export async function prepareRequest(up: string, source: string, token: string):
   const version = latestVersions[source];
 
   // encrypt secrets and upload to DON
-  const secretsManager = new SecretsManager({
-    signer,
-    functionsRouterAddress: process.env.DON_ROUTER || "",
-    donId: process.env.DON_ID_STRING || "",
-  });
-  await secretsManager.initialize();
-  const encryptedSecretsObj = await secretsManager.encryptSecrets({
-    token,
-    up: up.slice(2).toLowerCase(),
-    pinata: process.env.PINATA_JWT || "",
-  });
-  const gistURL = await createGist(process.env.GITHUB_API_TOKEN || "", JSON.stringify(encryptedSecretsObj));
-  const encryptedSecretsUrls = await secretsManager.encryptSecretsUrls([gistURL]);
+
+  // const secretsManager = new SecretsManager({
+  //   signer,
+  //   functionsRouterAddress: process.env.DON_ROUTER || "",
+  //   donId: process.env.DON_ID_STRING || "",
+  // });
+  // await secretsManager.initialize();
+  // const encryptedSecretsObj = await secretsManager.encryptSecrets({
+  //   token,
+  //   up: up.slice(2).toLowerCase(),
+  //   pinata: process.env.PINATA_JWT || "",
+  // });
+  // const gistURL = await createGist(process.env.GITHUB_API_TOKEN || "", JSON.stringify(encryptedSecretsObj));
+  // const encryptedSecretsUrls = await secretsManager.encryptSecretsUrls([gistURL]);
+
+  // TODO @chainlink/functions-toolkit as of now doesn't work with serverless architecture
+  const workaround = await axios.post(process.env.WORKAROUND_API + "/api/chainlink", {
+    up,
+    token
+  })
+
   // const uploadResult = await secretsManager.uploadEncryptedSecretsToDON({
   //     encryptedSecretsHexstring: encryptedSecretsObj.encryptedSecrets,
   //     gatewayUrls: [
@@ -38,7 +47,7 @@ export async function prepareRequest(up: string, source: string, token: string):
   //     throw new Error("Encrypted secrets not uploaded to DON");
   // }
   return {
-    secret: encryptedSecretsUrls,
+    secret: workaround.data.encryptedSecretsUrls,
     version,
   };
 }
